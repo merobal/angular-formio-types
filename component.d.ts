6f50f6279cba2768b734ad825c85ebe456a91391
gombosd <gombos.david96@gmail.com>
2019-07-03 16:36:13 +0200

    chore(component.d.ts): change into class FO-0

--- component.d.ts
+++ component.d.ts
@@ -1,33 +1,34 @@
-export namespace Component {
-  function on(event: string, cb: Function, internal: boolean): any;
-  function off(event: string): void;
-  function emit(event: string, data: Object): void;
-  function addEventListener(obj: HTMLElement, type: string, func: Function, persistent: boolean): any;
-  function removeEventListener(obj: Object, type: any): any;
-  function destroy(full: boolean): void;
-  function appendTo(element: any, container: any): any;
-  function prependTo(element: HTMLElement, container: HTMLElement): any;
-  function removeChildFrom(element: HTMLElement, container: HTMLElement): any;
-  function ce(type: string, attr?: Object, children?: HTMLElement | string | Array<HTMLElement | string>): HTMLElement;
-  function appendChild(element: any, child: any): any;
-  function maskPlaceholder(mask: HTMLElement): string;
-  function setInputMask(input: HTMLElement, inputMask: string, placeholder: boolean): void;
-  function t(text: string, params: Object): string;
-  function text(text: any): Text;
-  function attr(element: HTMLElement, attr: Object): void;
-  function hasClass(element: HTMLElement | any, className: string): boolean;
-  function addClass(element: HTMLElement, className: string): any;
-  function removeClass(element: HTMLElement, className: string): any;
-  function empty(element: HTMLElement): void;
-  function iconClass(name: string, spinning: boolean): string;
-  function getIcon(name: string): HTMLElement;
-  function evalContext(additional: any): any;
-  function interpolate(string: any, data: any): any;
-  function evaluate(
+export class Component {
+  constructor(options: Object, id: string);
+  on(event: string, cb: Function, internal: boolean): any;
+  off(event: string): void;
+  emit(event: string, data: Object): void;
+  addEventListener(obj: HTMLElement, type: string, func: Function, persistent: boolean): any;
+  removeEventListener(obj: Object, type: any): any;
+  destroy(full: boolean): void;
+  appendTo(element: any, container: any): any;
+  prependTo(element: HTMLElement, container: HTMLElement): any;
+  removeChildFrom(element: HTMLElement, container: HTMLElement): any;
+  ce(type: string, attr?: Object, children?: HTMLElement | string | Array<HTMLElement | string>): HTMLElement;
+  appendChild(element: any, child: any): any;
+  maskPlaceholder(mask: HTMLElement): string;
+  setInputMask(input: HTMLElement, inputMask: string, placeholder: boolean): void;
+  t(text: string, params: Object): string;
+  text(text: any): Text;
+  attr(element: HTMLElement, attr: Object): void;
+  hasClass(element: HTMLElement | any, className: string): boolean;
+  addClass(element: HTMLElement, className: string): any;
+  removeClass(element: HTMLElement, className: string): any;
+  empty(element: HTMLElement): void;
+  iconClass(name: string, spinning: boolean): string;
+  getIcon(name: string): HTMLElement;
+  evalContext(additional: any): any;
+  interpolate(string: any, data: any): any;
+  evaluate(
     func: any,
     args: { component: any; form: any; instance: any; row: any; data: any } | any,
     ret: any,
     tokenize: any,
   ): any;
-  function hook(): any;
+  hook(): any;
 }
